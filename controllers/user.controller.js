438b4ad9de78b2235ae83966a340a1fb5b0cb396
Vivek <bunkarvivek75@gmail.com>
2025-03-02 04:55:25 +0530

    Added: JWT Authentication

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -18,9 +18,9 @@ async function handleUserLogin(req, res) {
   if (!user)
     return res.render("login", { error: "Invalid username or password" });
 
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+  // const sessionId = uuidv4();
+  const token = setUser(user);
+  res.cookie("uid", token);
 
   return res.redirect("/");
 }
--- service/auth.js
+++ service/auth.js
@@ -1,14 +1,37 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user)
+// function setUser(id, user){
+//     sessionIdToUserMap.set(id, user)
+// }
+
+// function getUser(id){
+//     return sessionIdToUserMap.get(id);
+// }
+
+// module.exports = {
+//     setUser,
+//     getUser
+// }
+
+const jwt = require("jsonwebtoken");
+const secret = "Vivek@123";
+
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  return jwt.verify(token, secret);
 }
 
 module.exports = {
-    setUser,
-    getUser
-}
+  setUser,
+  getUser,
+};
